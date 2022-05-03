bb75adbd8006b705eeb8e1bf43e7bc80bee8af45
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2022-05-03 13:22:52 -0400

    Added Explore #001.

---
+++ explore/Ex001-EqualsStrategy/src/test/java/org/patrodyne/jvnet/basicjaxb/ex001/Explorer.java
@@ -0,0 +1,446 @@
+package org.patrodyne.jvnet.basicjaxb.ex001;
+
+import static java.lang.Integer.toHexString;
+import static java.lang.System.identityHashCode;
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
+import static javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT;
+
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.TransformerException;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.patrodyne.jvnet.basicjaxb.ex001.model.Person;
+import org.patrodyne.jvnet.basicjaxb.explore.AbstractExplorer;
+import org.patrodyne.jvnet.basicjaxb.validation.SchemaOutputDomResolver;
+import org.patrodyne.jvnet.basicjaxb.validation.SchemaOutputStringResolver;
+import org.xml.sax.SAXException;
+
+/**
+ * A Swing application to explore features of the HiSrc BasicJAXB Runtime library.
+ * 
+ * Projects create their own custom Explorer by extending AbstractExplorer and
+ * providing an HTML lesson page then adding JMenuItem(s) to trigger exploratory code.
+ * 
+ * The AbstractExplorer displays three panels: an HTML lesson, a print console and an
+ * error console. The lesson file is read as a resource relative to this class. Text
+ * is sent to the print console by calling 'println(text)' and error messages are
+ * sent to the error console by calling 'errorln(msg)'. Also, 'standard out' /
+ * 'standard error' streams are sent to their respective consoles.
+ * 
+ * @author Rick O'Sullivan
+ */
+@SuppressWarnings("serial")
+public class Explorer extends AbstractExplorer
+{
+	private static final String WINDOW_TITLE = "HiSrc BasicJAXB Ex001 EqualsStrategy";
+	private static final String EXPLORER_HTML = "Explorer.html";
+
+	private JAXBContext jaxbContext;
+	public JAXBContext getJaxbContext() { return jaxbContext; }
+	public void setJaxbContext(JAXBContext jaxbContext) { this.jaxbContext = jaxbContext; }
+
+	private Marshaller marshaller;
+	public Marshaller getMarshaller() { return marshaller; }
+	public void setMarshaller(Marshaller marshaller) { this.marshaller = marshaller; }
+
+	private Unmarshaller unmarshaller;
+	public Unmarshaller getUnmarshaller() { return unmarshaller; }
+	public void setUnmarshaller(Unmarshaller unmarshaller) { this.unmarshaller = unmarshaller; }
+	
+	private Person person1;
+	public Person getPerson1() { return person1; }
+	public void setPerson1(Person person1) { this.person1 = person1; }
+
+	private Person person2;
+	public Person getPerson2() { return person2; }
+	public void setPerson2(Person person2) { this.person2 = person2; }
+
+	private Person person3;
+	public Person getPerson3() { return person3; }
+	public void setPerson3(Person person3) { this.person3 = person3; }
+
+	/**
+	 * Main entry point for command line invocation.
+	 * @param args CLI arguments
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new Explorer(EXPLORER_HTML);
+			frame.setVisible(true);
+		});
+	}
+
+	/**
+	 * Construct application with an HTML lesson.
+	 */
+	public Explorer(String htmlName)
+	{
+		super(htmlName);
+		setTitle(WINDOW_TITLE);
+		try
+		{
+			initializeExplorer();
+			setJMenuBar(createMenuBar());
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	public void generateXmlSchemaFromString()
+	{
+		try
+		{
+			SchemaOutputStringResolver sosr = new SchemaOutputStringResolver();
+			getJaxbContext().generateSchema(sosr);
+			println("Xml Schema from String:\n\n" + sosr.getSchemaString());
+		}
+		catch ( IOException ex )
+		{
+			errorln(ex);
+		}
+	}
+	
+	public void generateXmlSchemaFromDom()
+	{
+		try
+		{
+			SchemaOutputDomResolver sodr = new SchemaOutputDomResolver();
+			getJaxbContext().generateSchema(sodr);
+			println("Xml Schema from DOM:\n\n" + sodr.getSchemaDomNodeString());
+		}
+		catch ( IOException | TransformerException ex )
+		{
+			errorln(ex);
+		}
+	}
+	
+	public void generateXmlSchemaValidatorFromDom()
+	{
+		try
+		{
+			// Generate a Schema Validator from given the JAXB context.
+			SchemaOutputDomResolver sodr = new SchemaOutputDomResolver();
+			getJaxbContext().generateSchema(sodr);
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI);
+			Schema schemaValidator = schemaFactory.newSchema(sodr.getDomSource());
+			
+			// Configure Marshaller / unmarshaller to use validator.
+			getMarshaller().setSchema(schemaValidator);
+			getUnmarshaller().setSchema(schemaValidator);
+			
+			println("Schema Validator:\n\n" + schemaValidator);
+			println();
+		}
+		catch ( IOException | SAXException ex )
+		{
+			errorln(ex);
+		}
+	}
+	
+	public void marshalPerson1()
+	{
+		marshal("Person1", getPerson1());
+	}
+	
+	public void marshalPerson2()
+	{
+		marshal("Person2", getPerson2());
+	}
+	
+	public void marshalPerson3()
+	{
+		marshal("Person3", getPerson3());
+	}
+
+	private void marshal(String label, Person person)
+	{
+		try
+		{
+			String phc = toHexString(person.hashCode());
+			String ihc = toHexString(identityHashCode(person));
+			String personXml = marshalToString(person);
+			println(label + " XML: (P#=" + phc + ", O#=" + ihc + ")\n\n" +personXml);
+			println();
+		}
+		catch (JAXBException | IOException ex)
+		{
+			errorln(ex);
+		}
+	}
+	
+	public void compareHashCodes()
+	{
+		String person1HashCode = toHexString(getPerson1().hashCode());
+		String person2HashCode = toHexString(getPerson2().hashCode());
+		String person3HashCode = toHexString(getPerson3().hashCode());
+
+		String person1IdentityHashCode = toHexString(identityHashCode(getPerson1()));
+		String person2IdentityHashCode = toHexString(identityHashCode(getPerson2()));
+		String person3IdentityHashCode = toHexString(identityHashCode(getPerson3()));
+		
+		println("Compare Hash Codes\n");
+		println("Person1 hashCode: " + person1HashCode + "; identityHashCode: " + person1IdentityHashCode);
+		println("Person2 hashCode: " + person2HashCode + "; identityHashCode: " + person2IdentityHashCode);
+		println("Person3 hashCode: " + person3HashCode + "; identityHashCode: " + person3IdentityHashCode);
+		println();
+	}
+	
+	public void compareEquality()
+	{
+		println("Compare Equality\n");
+		println("Person1 vs Person2: " + (getPerson1().equals(getPerson2()) ? "EQUAL" : "UNEQUAL"));
+		println("Person1 vs Person3: " + (getPerson1().equals(getPerson3()) ? "EQUAL" : "UNEQUAL"));
+		println();
+	}
+
+	public void compareToString()
+	{
+		println("Compare ToString\n");
+		println("Person1 toString: " + getPerson1().toString());
+		println("Person2 toString: " + getPerson2().toString());
+		println("Person3 toString: " + getPerson3().toString());
+		println();
+	}
+
+	public void roundtripValid()
+	{
+		try
+		{
+			Person person1A = getPerson1();
+			String person1AXml = marshalToString(person1A);
+			Person person1B = unmarshalFromString(person1AXml);
+			println("Person1A vs Person1B: " + (person1A.equals(person1B) ? "EQUAL" : "UNEQUAL"));
+			println();
+		}
+		catch (JAXBException | IOException ex)
+		{
+			errorln(ex);
+		}
+	}
+
+	public void roundtripInvalid()
+	{
+		try
+		{
+			// person1BXml is intentionally invalid!
+			Person person1A = getPerson1();
+			String person1AXml = marshalToString(person1A);
+			String person1BXml = person1AXml.replaceAll("LastName", "BabelFish");
+			Person person1B = unmarshalFromString(person1BXml);
+			if ( person1A.equals(person1B) )
+				println("Person1A vs Person1B: EQUAL");
+			else
+			{
+				println("Person1A vs Person1B: UNEQUAL");
+				println();
+				println("[BEFORE] Person1A XML:\n" + person1AXml);
+				println("[BEFORE] Person1B XML:\n" + person1BXml);
+				println();
+				println("[AFTER] Person1A XML:\n" + marshalToString(person1A));
+				println("[AFTER] Person1B XML:\n" + marshalToString(person1B));
+			}
+			println();
+		}
+		catch (JAXBException | IOException ex)
+		{
+			errorln(ex);
+		}
+	}
+
+	/**
+	 * Dispatch hyperlinks from the lesson to local method invocations.
+	 * The markdown for hyperlinks in the lesson is declared like this:
+	 * 
+	 *   [description](!hyperLink)
+	 */
+	@Override
+	public void dispatchHyperLink(String hyperLink)
+	{
+		switch ( hyperLink )
+		{
+			case "generateXmlSchemaFromString": generateXmlSchemaFromString(); break;
+			case "generateXmlSchemaFromDom": generateXmlSchemaFromDom(); break;
+			case "generateXmlSchemaValidatorFromDom": generateXmlSchemaValidatorFromDom(); break;
+			case "marshalPerson1": marshalPerson1(); break;
+			case "marshalPerson2": marshalPerson2(); break;
+			case "marshalPerson3": marshalPerson3(); break;
+			case "compareHashCodes": compareHashCodes(); break;
+			case "compareEquality": compareEquality(); break;
+			case "compareToString": compareToString(); break;
+			case "roundtripValid": roundtripValid(); break;
+			case "roundtripInvalid": roundtripInvalid(); break;
+		}
+	}
+
+	/*
+	 * Create a JMenuBar to display JMenuItem(s) to trigger
+	 * your own exploratory methods.
+	 */
+	public JMenuBar createMenuBar()
+	{
+		JMenuBar menuBar = new JMenuBar();
+		
+		// Context Menu
+		{
+			JMenu contextMenu = new JMenu("Context");
+			// Context: Generate XML Schema from String
+			{
+				JMenuItem menuItem = new JMenuItem("Generate XML Schema from String");
+				menuItem.addActionListener((event) -> generateXmlSchemaFromString());
+				contextMenu.add(menuItem);
+			}
+			// Context: Generate XML Schema from DOM
+			{
+				JMenuItem menuItem = new JMenuItem("Generate XML Schema from DOM");
+				menuItem.addActionListener((event) -> generateXmlSchemaFromDom());
+				contextMenu.add(menuItem);
+			}
+			// Context: Generate XML Schema Validator from DOM
+			{
+				JMenuItem menuItem = new JMenuItem("Generate XML Schema Validator from DOM");
+				menuItem.addActionListener((event) -> generateXmlSchemaValidatorFromDom());
+				contextMenu.add(menuItem);
+			}
+			menuBar.add(contextMenu);
+		}
+
+		// Marshal Menu
+		{
+			JMenu marshalMenu = new JMenu("Marshal");
+			// Marshal: Person1
+			{
+				JMenuItem menuItem = new JMenuItem("Person1");
+				menuItem.addActionListener((event) -> marshalPerson1());
+				marshalMenu.add(menuItem);
+			}
+			// Marshal: Person2
+			{
+				JMenuItem menuItem = new JMenuItem("Person2");
+				menuItem.addActionListener((event) -> marshalPerson2());
+				marshalMenu.add(menuItem);
+			}
+			// Marshal: Person3
+			{
+				JMenuItem menuItem = new JMenuItem("Person3");
+				menuItem.addActionListener((event) -> marshalPerson3());
+				marshalMenu.add(menuItem);
+			}
+			menuBar.add(marshalMenu);
+		}
+
+		// Compare Menu
+		{
+			JMenu compareMenu = new JMenu("Compare");
+			// Compare: HashCodes
+			{
+				JMenuItem menuItem = new JMenuItem("HashCodes");
+				menuItem.addActionListener((event) -> compareHashCodes());
+				compareMenu.add(menuItem);
+			}
+			// Compare: Equality
+			{
+				JMenuItem menuItem = new JMenuItem("Equality");
+				menuItem.addActionListener((event) -> compareEquality());
+				compareMenu.add(menuItem);
+			}
+			// Compare: ToString
+			{
+				JMenuItem menuItem = new JMenuItem("ToString");
+				menuItem.addActionListener((event) -> compareToString());
+				compareMenu.add(menuItem);
+			}
+			menuBar.add(compareMenu);
+		}
+
+		// Roundtrip Menu
+		{
+			JMenu roundtripMenu = new JMenu("Roundtrip");
+			// Roundtrip: Valid
+			{
+				JMenuItem menuItem = new JMenuItem("Valid");
+				menuItem.addActionListener((event) -> roundtripValid());
+				roundtripMenu.add(menuItem);
+			}
+			// Roundtrip: Invalid
+			{
+				JMenuItem menuItem = new JMenuItem("Invalid");
+				menuItem.addActionListener((event) -> roundtripInvalid());
+				roundtripMenu.add(menuItem);
+			}
+			menuBar.add(roundtripMenu);
+		}
+
+		return menuBar;
+	}
+	
+	private void initializeExplorer() throws Exception
+	{
+		setJaxbContext(createJAXBContext());
+		setMarshaller(createMarshaller(getJaxbContext()));
+		setUnmarshaller(createUnmarshaller(getJaxbContext()));
+		setPerson1(createPerson("Arthur", "Dent"));
+		setPerson2(createPerson("Arthur", "Dent"));
+		setPerson3(createPerson("Ford", "Prefect"));
+	}
+
+	private Person createPerson(String firstName, String lastName)
+	{
+		Person person = new Person();
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		return person;
+	}
+	
+	private JAXBContext createJAXBContext() throws JAXBException
+	{
+		return JAXBContext.newInstance(Person.class);
+	}
+
+	protected Marshaller createMarshaller(JAXBContext jaxbContext) throws JAXBException
+	{
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(JAXB_FORMATTED_OUTPUT, true);
+		return marshaller;
+	}
+	
+	protected Unmarshaller createUnmarshaller(JAXBContext jaxbContext) throws JAXBException
+	{
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		return unmarshaller;
+	}
+
+	protected String marshalToString(Object instance) throws JAXBException, IOException
+	{
+		try ( StringWriter writer = new StringWriter() )
+		{
+			getMarshaller().marshal(instance, writer);
+			return writer.toString();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> T unmarshalFromString(String xml) throws JAXBException
+	{
+		try ( StringReader reader = new StringReader(xml) )
+		{
+			Object instance = getUnmarshaller().unmarshal(reader);
+			return (T) instance;
+		}
+	}
+}
---
+++ explore/Ex001-EqualsStrategy/src/test/resources/org/patrodyne/jvnet/basicjaxb/ex001/Explorer.md
@@ -0,0 +1,168 @@
+# HiSrc BasicJAXB Ex001 EqualsStrategy
+
+This project is the first exploration of the HiSrc BasicJAXB library. It includes a Swing application named _Explorer_ to demonstrate two features of the HiSrc BasicJAXB Runtime library: `JAXBHashCodeStrategy` and `JAXBEqualsStrategy`. This _Explorer_ application is designed to present a narrative lesson together with dynamic output for real-time experimentation. Feel free to modify the _Explorer.java_ source file to add or modify the action methods with your own investigative code. The `Explorer` class is an extension of `AbstractExplorer` which contains the more boring mechanics of this implementation. Feel free to create an `Explorer` class in your own projects to help explain the purpose of your work too.
+
+> **About AbstractExplorer:** Projects create their own custom Explorer by extending `AbstractExplorer` and providing an HTML lesson page plus JMenuItem(s) to trigger exploratory code.
+
+> An `AbstractExplorer` implementation (like the one you see here) displays three panels: an HTML lesson, a print console and an error console. The lesson file is read as a resource relative to the implementation (i.e. `Explorer`) class. Text is sent to the print console by calling `println(text)` and error messages are sent to the error console by calling `errorln(msg)`. Additionally, 'standard out' / 'standard error' streams are sent to their respective consoles.
+
+## Object Method Strategies
+
+The focus of the HiSrc framework is the automatic generation of Java classes from XML
+Schema(s). The XJC compiler from JAXB does the actual code generation using plug-ins from the HiSrc framework. Then JAXB is used to unmarshal/marshal XML instances to/from Java objects. 
+
+XJC generates POJO classes with the standard and simple Java `Object` methods:
+
++ `hashCode()` - numeric value used for object identification or indexing. 
++ `equals(obj)` - determines when and how to objects are equal.
++ `toString()` - textual representation of an object.
+
+By default, these methods work at the object level. For example, two distinct `Employee` object instances are considered _not equal_ even when they contain the same employee values because they have different object identifiers. The _HiSrc BasicJAXB Runtime Library_ provides strategies to implement these and other `Object` methods that take all values into account.
+
+## Explore HashCode and Equals
+
+Let's [explore][1] two strategies provided by this library: `JAXBHashCodeStrategy` and `JAXBEqualsStrategy`. We'll look at the other strategies in future explorations.
+
+> **Hint:** As we cover each topic, you can run the code by clicking on the links or using the menu bar above.
+
+The HiSrc framework uses its Maven plugin(s) to generate Java classes from XML schema files.
+The schema file for this project is in the `src/main/resources/Person.xsd` file. To keep the design simple, the schema declares `Person` type to be an XML element with an anonymous complex type and the `Person` type and declares only two sub-elements: `FirstName` and `LastName`. The POM for this project configures this Maven plug-in to generate the JAXB source code for the `Person` type. Also, the Maven plug-in is configured to include the `equals` and `hashCode` XJC plug-ins from our HiSrc framework.
+
+~~~
+<plugin>
+    <groupId>org.patrodyne.jvnet</groupId>
+    <artifactId>hisrc-higherjaxb-maven-plugin</artifactId>
+    ...
+        <configuration>
+            <args>
+                <arg>-no-header</arg>
+                <arg>-Xequals</arg>
+                <arg>-XhashCode</arg>
+            </args>
+            ...
+        </configuration>
+</plugin>
+~~~
+
+The plug-in generates a `Person` class that implements the HiSrc interfaces (`HashCode2`, `Equals2`) for the `JAXBHashCodeStrategy` and `JAXBEqualsStrategy`. It overrides the relevant `Object` methods with the custom strategies. This implementation introduces another HiSrc interface, named `ObjectLocator`.
+
+>**Note:** By convention, when XJC parses a top-level element with a anonymous complex type, it will add the `@XmlRootElement` annotation to the generated class.
+
+### ObjectLocator
+
+The `ObjectLocator` interface extends `javax.xml.bind.ValidationEventLocator` used by JAXB to report XML schema (in)validation events. It does this by adding a path from the event back to the root object. HiSrc strategies use the `ObjectLocator` to trace the location of events like a _field not equal_ event. For example, `DefaultEqualsStrategy` delegates the event notification to [SLF4J][2], for logging. Applications can configure [SLF4J][2] to ignore all reporting or override the default tracing with their own custom event coding.
+
+>**Hint:** The POM for this project declares the `org.slf4j:slf4j-simple` dependency to implement the [SLF4J][2] API. This implementation is configured in the `src/test/resources/simplelogger.properties` file. Event notification can be activated by setting the logger level to `...JAXBEqualsStrategy=TRACE`.
+
+The locators are optional. `ObjectLocator` parameters can be null when invoking the HiSrc strategies. When not used, the strategies report a more general location message.
+
+> Normally, applications configure [SLF4J][2] to ignore the `ObjectLocator` events. Applications can choose to report events, when needed, to debug unexpected inequality issues. They are most commonly used in unit tests to verify round trip conformance (_XML instance_ to _Java Object_ to _XML instance_).
+
+### Regenerate Xml Schema
+
+This example starts with a  `Person` element and anonymous type in the XML Schema then generates the Java class. We can use the `Person` class to create a JAXB context.
+
+~~~
+private JAXBContext createJAXBContext() throws JAXBException
+{
+    return JAXBContext.newInstance(Person.class);
+}
+~~~
+
+The `JAXBContext` class provides the application's entry point to the JAXB API. It provides an abstraction for managing the XML/Java binding information necessary to implement the JAXB binding framework operations: _unmarshal_, _marshal_ and _validate_. For display, we use JAXB's `SchemaOutputResolver` to generate a textual representation in two ways:
+
++ [Regenerate Xml Schema From String](!generateXmlSchemaFromString)
++ [Regenerate Xml Schema From Dom](!generateXmlSchemaFromDom)
+
+The first way (above) outputs the XML Schema to a `String` backed `SchemaOutputResolver`. The second way outputs the XML Schema to a `DOMSource` backed resolver. The output from both approaches are similar but the second way enables us to generate a _schema validator object_ that can be wired into the marshaller and unmarshaller objects.
+
++ [Generate Xml Schema Validator From Dom](!generateXmlSchemaValidatorFromDom)
+
+> **Note:** This exploration does not wire in the _schema validator object_ automatically. This allows you explore the effect of unmarshalling an invalid schema, as described below. If you generate the validator, it will be active for the current session. Restart this application to clear the validator and explore the other actions below.
+
+### Marshal Person Objects to XML
+
+For this demo, `Person` is a JAXB annotated class with fields for first and last name. The demo is initialized with three instances: 
+
++ [Person 1, Arthur Dent](!marshalPerson1)
++ [Person 2, Arthur Dent](!marshalPerson2)
++ [Person 3, Ford Prefect](!marshalPerson3)
+
+All three instances are distinct Java objects. The first two objects refer to "Arthur Dent" and the third object identifies "Ford Prefect".
+
+### Compare Objects
+
+The HiSrc framework provides an `ObjectLocator` and `HashCodeStrategy2` to generate a hash code that is based on the object values. In this case, the hash code and equality is based on the person's first and last name. 
+
+#### Compare Hash Codes
+
+Select [Compare Hash Codes](!compareHashCodes) to show the HiSrc hash and the System hash for the three `Person` instances. The HiSrc hash codes for Person 1 & 2 are the same because these two objects both identify "Arthur Dent" while the hash code for Person #3 is different from the others because that object identifies "Ford Prefect". However, the system hash codes for all three objects are unique because they are individual JVM objects.
+
+Here's how the `Person` class uses the HiSrc interfaces for the `hashCode()` method. See [Explorer.java][1] for the field level details.
+
+~~~
+public int hashCode()
+{
+    ObjectLocator thisLocator = new DefaultRootObjectLocator(this);
+    HashCodeStrategy2 strategy = JAXBHashCodeStrategy.getInstance();
+    return this.hashCode(thisLocator, strategy);
+}
+~~~
+
+#### Compare Equality
+
+Select [Compare Equality](!compareEquality) to compare (A) `person1` with `person2` and (B) `person2` with `person3`.
+
+Case (A) compares two individual objects for "Arthur Dent" and finds that they are equal.
+
+Case (B) compares an "Arthur Dent" object with a "Ford Prefect" object and finds that they are _not_ equal. Further, case (B) provides a trace log to report the first non-equal field.
+
+Here's how the `Person` class uses the HiSrc interfaces for the `equals(Object that)` method. See [Explorer.java][1] for the field level details.
+
+> **Note:** The `ObjectLocator` parameters are optional; they can be null. When they are null, the field level locator utility will use basic location tracing. When the root level locators are used, the field level locators create a nested path that is used for trace level logging. HiSrc uses [SLF4J][2] as its logging framework and that framework is usually configured to skip trace level messaging.
+
+~~~
+public boolean equals(Object that)
+{
+    // Create locators for enhanced debugging.
+    ObjectLocator thisLocator = new DefaultRootObjectLocator(this);
+    ObjectLocator thatLocator = new DefaultRootObjectLocator(that);
+    
+    // Return deep object equality.
+    JAXBEqualsStrategy strategy = new JAXBEqualsStrategy();
+    return equals(thisLocator, thatLocator, that, strategy);
+}
+~~~
+
+> **Important:** The HiSrc `JAXBEqualsStrategy` extends `DefaultEqualsStrategy` and both include logic to differentiate objects that use _default_ vs _set_ values. In JAXB, an effort is made to track how an element or attribute receives its value. If the XML Schema defines a _default_ value for an element or attribute and the XML instance does not set the value then the JAXB generated class will return the default value from the accessor while keeping the field value null. This may surprise a maintainer when reviewing the Java code but it is how JAXB is designed to work! This allows JAXB to detect when a property is a **set value** versus a **default value** and can help with round tripping.
+
+#### Compare ToString
+
+Select [Compare ToString](!compareToString) to compare `person1`, `person2` and `person3`.
+
+### Roundtrip
+
+In JAXB, a _round trip_ is an _xml-to-object-to-xml_ or an _object-to-xml-to-object_ sequence. Ideally, the initial XML and final XML instance will be the same. There are mechanisms, such as `JAXBElement`, that can be used to preserve the fine details and are often used. However, most projects do not require an exact reproduction of the XML and are satisfied with being `equal-by-value`.
+
+#### Valid Roundtrip
+
+The `JAXBEqualsStrategy` can be used to verify deep `equal-by-value`. Select [Roundtrip Valid](!roundtripValid) to marshal the `person1` object into its XML representation then unmarshal the XML back into a second object; finally, this test will use the object's equal method to verify equal by value.
+
+#### Invalid Roundtrip
+
+Select [Roundtrip Invalid](!roundtripInvalid) to create a bit of chaos. This exploration intentionally corrupts the XML by replacing the `lastName` tag with a random `babelFish` tag. Because of this, the corrupted XML will not have a `lastName` value but will have unknown tag from the XML Schema point of view.
+
+If you have not selected _Generate Xml Schema Validator From Dom_ then JAXB will attempt to complete the roundtrip but the final XML will contain this bit of chaos:
+
+~~~
+<LastName>lastName</LastName>
+~~~
+
+To detect this chaos programatically, use [Generate Xml Schema Validator From Dom](!generateXmlSchemaValidatorFromDom) then execute [Roundtrip Invalid](!roundtripInvalid). This time, JAXB validation will throw an exception.
+
+## End of this Exploration
+
+<!-- References -->
+
+[1]: https://github.com/patrodyne/hisrc-basicjaxb/blob/master/runtime/src/test/java/org/patrodyne/jvnet/basicjaxb/ex001/Explorer.java
+[2]: https://www.slf4j.org/
