f8ac0784553102ce40cc97356f71ee544c4e45b1
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2022-05-22 15:28:29 -0400

    Added PlantUML/Graphviz diagramming.

---
+++ explore/Ex001-EqualsStrategy/project-plantuml.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Use PlantUML to generate class diagram.
+# See https://sourceforge.net/projects/plantuml/files/plantuml.jar/download
+# See https://graphviz.org/download/
+#
+# Configure BEGIN
+export GRAPHVIZ_DOT="/usr/bin/dot"
+IMGTYPE="svg"
+PLANTUML_HOME="/opt/java/plantuml"
+PLANTUML_TEMP="target/generated-docs/PersonTemp.puml"
+PLANTUML_FILE="target/generated-docs/Person.puml"
+PLANTUML_DEST="src/main/resources/Person.${IMGTYPE}"
+# Configure END
+
+# Pre-process PUML
+cat "${PLANTUML_TEMP}" | \
+	sed 's/java.math.BigDecimal/BigDecimal/' | \
+	sed 's/javax.xml.datatype.XMLGregorianCalendar/XMLGregorianCalendar/' \
+	>"${PLANTUML_FILE}"
+
+# Generate IMG
+java -jar ${PLANTUML_HOME}/plantuml.jar -t${IMGTYPE} "${PLANTUML_FILE}"
+PLANTUML_IMG="${PLANTUML_FILE%.puml}.${IMGTYPE}"
+echo "PlantUML generated ${PLANTUML_IMG}"
+mv "${PLANTUML_IMG}" "${PLANTUML_DEST}"
+echo "Installed to ${PLANTUML_DEST}"
