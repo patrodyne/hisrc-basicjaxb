496d6d4ec6451ae6e7816ad23d2783cf55925d42
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-07-19 16:20:25 -0400

    Added ClassUtils methods.

--- runtime/src/main/java/org/jvnet/basicjaxb/lang/ClassUtils.java
+++ runtime/src/main/java/org/jvnet/basicjaxb/lang/ClassUtils.java
@@ -1,5 +1,15 @@
 package org.jvnet.basicjaxb.lang;
 
+import static java.lang.Class.forName;
+import static java.lang.ClassLoader.getSystemClassLoader;
+import static java.util.stream.Collectors.toSet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+
 public class ClassUtils
 {
 	public static final char PACKAGE_SEPARATOR_CHAR = '.';
@@ -58,5 +68,31 @@ public class ClassUtils
 			}
 		}
 		return new String(chars, lastDot, chars.length - lastDot);
+	}
+	
+	public static Set<Class<?>> findClasses(Package pkg) throws IOException
+	{
+		String pkgName = pkg.getName();
+		try ( InputStream stream = getSystemClassLoader().getResourceAsStream(pkgName.replaceAll("[.]", "/")) )
+		{
+			try ( BufferedReader reader = new BufferedReader(new InputStreamReader(stream)) )
+			{
+				return reader.lines()
+					.filter(line -> line.endsWith(".class")).map(line -> getClass(line, pkgName)).collect(toSet());
+			}
+		}
+	}
+	
+	private static Class<?> getClass(String className, String pkgName)
+	{
+		try
+		{
+			return forName(pkgName + "." + className.substring(0, className.lastIndexOf('.')));
+		}
+		catch (ClassNotFoundException e)
+		{
+			// handle the exception
+			return null;
+		}
 	}
 }
