011b61e339bb07eb1d85216cdfda0920c002f665
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-08-06 13:58:18 -0400

    Formatting

--- tools/src/main/java/org/jvnet/basicjaxb/reflection/util/Accessor.java
+++ tools/src/main/java/org/jvnet/basicjaxb/reflection/util/Accessor.java
@@ -1,9 +1,7 @@
 package org.jvnet.basicjaxb.reflection.util;
 
-public interface Accessor<T> {
-
+public interface Accessor<T>
+{
 	public T get(Object target);
-
 	public void set(Object target, T value);
-
 }
