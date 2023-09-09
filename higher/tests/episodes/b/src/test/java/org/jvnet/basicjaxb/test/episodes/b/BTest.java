44abe67f2454230e4118d8d327c70d05d65c1481
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-09-09 12:08:27 -0400

    Added FixOtherAttributes plugin

---
+++ higher/tests/episodes/b/src/test/java/org/jvnet/basicjaxb/test/episodes/b/BTest.java
@@ -0,0 +1,94 @@
+package org.jvnet.basicjaxb.test.episodes.b;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+
+import org.jvnet.basicjaxb.lang.ContextUtils;
+import org.jvnet.basicjaxb.test.AbstractSamplesTest;
+import org.jvnet.basicjaxb.test.episodes.a.A1Type;
+import org.jvnet.basicjaxb.test.episodes.a.AType;
+import org.jvnet.basicjaxb.test.episodes.a.IssueHJIII24Animal;
+import org.jvnet.basicjaxb.test.episodes.a.XType;
+import org.w3c.dom.Element;
+
+import jakarta.xml.bind.JAXBElement;
+
+public class BTest extends AbstractSamplesTest
+{
+	private static final ObjectFactory OF = new ObjectFactory();
+	
+    @Override
+    protected String getContextPath()
+    {
+        return ContextUtils.getContextPath(OF.getClass());
+    }
+    
+	@Override
+	protected void checkSample(File sample) throws Exception
+	{
+		Object object = createContext().createUnmarshaller().unmarshal(sample);
+
+		if ( object instanceof JAXBElement)
+			object = ((JAXBElement<?>) object).getValue();
+
+		if ( object instanceof BType )
+		{
+			BType bOne = new BType();
+			{
+				A1Type a1 = new A1Type();
+				a1.setA1("test");
+				bOne.setA(a1);
+				B1Type b1 = new B1Type();
+				b1.setB1("test");
+				bOne.setB(b1);
+			}
+			BType bTwo = (BType) object;
+			getLogger().debug("bOne: {}", bOne);
+			getLogger().debug("bTwo: {}", bTwo);
+			assertEquals(bOne, bTwo, "Expected BType and unmarshaled BType are equal.");
+		}
+		else if ( object instanceof AType )
+		{
+			AType aOne = new AType();
+			{
+				A1Type a1 = new A1Type();
+				a1.setA1("test");
+				aOne.setA(a1);
+			}
+			AType aTwo = (AType) object;
+			getLogger().debug("aOne: {}", aOne);
+			getLogger().debug("aTwo: {}", aTwo);
+			assertEquals(aOne, aTwo, "Expected AType and unmarshaled AType are equal.");
+		}
+		else if ( object instanceof IssueHJIII24Animal )
+		{
+			IssueHJIII24Animal animal1 = new IssueHJIII24Animal();
+			animal1.setName("test");
+			IssueHJIII24Animal animal2 = (IssueHJIII24Animal) object;
+			getLogger().debug("animal1: {}", animal1);
+			getLogger().debug("animal2: {}", animal2);
+			assertEquals(animal1, animal2, "Expected animal and unmarshaled animal are equal.");
+		}
+		else if ( object instanceof XType )
+		{
+			XType x1 = new XType();
+			{
+				x1.setX1("x1");
+				x1.setX2("x2");
+			}
+			XType x2 = (XType) object;
+			{
+				// kludge
+				Element test = x2.getAny().get(0);
+				x1.getAny().add(test);
+			}
+			getLogger().debug("x1: {}", x1);
+			getLogger().debug("x2: {}", x2);
+			assertEquals(x1, x2, "Expected XType and unmarshaled XType are equal.");
+		}
+		else
+			fail("Unknown type: " + object);
+	}
+}
---
+++ plugins/src/main/java/org/jvnet/basicjaxb/plugin/fixotherattributes/FixOtherAttributes.java
@@ -0,0 +1,183 @@
+package org.jvnet.basicjaxb.plugin.fixotherattributes;
+
+import static java.lang.String.format;
+import static org.jvnet.basicjaxb.util.LocatorUtils.toLocation;
+
+import java.lang.reflect.Field;
+import java.util.Map.Entry;
+
+import org.jvnet.basicjaxb.plugin.AbstractPlugin;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JType;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.Outline;
+
+/**
+ * <p>
+ * This plugin fixes an issue when using episodes and extending <code>xs:anyType</code>. An extension of
+ * <code>xs:anyType</code> generates a field named <code>otherAttributes</code> with the annotation
+ * <code>@XmlAnyAttribute</code>. An extension of the extension should inherit the field and its
+ * getter/setter methods and this is the behavior when XJC is run in a single episode. But, as of about
+ * XJC 4.0.2, binding with an episode causes the second extension to duplicate and override the first
+ * <code>otherAttributes</code> field and its annotation <code>@XmlAnyAttribute</code>. This leads to:
+ * </p>
+ * 
+ * {@code}<pre>
+ * org.glassfish.jaxb.runtime.v2.runtime.IllegalAnnotationsException:
+ * Can't have @XmlAnyAttribute when a base class has it already.
+ * </pre>{@code}
+ * 
+ * <p>
+ * This plugin iterates over the class outlines and removes duplicate <code>otherAttributes</code>
+ * field(s) plus the field's associated <code>@XmlAnyAttribute</code>, <code>getter</code> and
+ * <code>setter</code> methods.
+ * </p>
+ * 
+ * <p>
+ * <b>Note:</b> This plugin requires JAR(s) from previous episodes be added as dependencies
+ * to the XJC classpath. In particular, as dependencies to the Maven plugin used to run XJC.
+ * </p>
+ * 
+ * @see <a href="https://github.com/eclipse-ee4j/jaxb-ri/issues/1735">JAXB-RI-1735</a>
+ * @see <a href="https://github.com/eclipse-ee4j/jaxb-ri/issues/1356">JAXB-RI-1356</a>
+ */
+public class FixOtherAttributes extends AbstractPlugin
+{
+	/** Name of Option to enable this plugin. */
+	private static final String OPTION_NAME = "XfixOtherAttributes";
+	
+	/** Description of Option to enable this plugin. */
+	private static final String OPTION_DESC = "removes duplicate otherAttributes property";
+
+	@Override
+	public String getOptionName()
+	{
+		return OPTION_NAME;
+	}
+
+	@Override
+	public String getUsage()
+	{
+		return format(USAGE_FORMAT, OPTION_NAME, OPTION_DESC);
+	}
+
+	/** Represents the JAXB/XJC generated  "otherAttributes" field */
+	private static final String OTHER_ATTRIBUTES = "otherAttributes";
+
+	/** Represents the JAXB/XJC generated  "getOtherAttributes" method */
+	private static final String GET_OTHER_ATTRIBUTES = "getOtherAttributes";
+
+	/** Represents a no-argument method signature */
+	private static final JType[] NO_ARGS = new JType[0];
+
+	// Plugin Processing
+	
+	@Override
+	protected void beforeRun(Outline outline) throws Exception
+	{
+		if ( isInfoEnabled() )
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(LOGGING_START);
+			sb.append("\nParameters");
+			sb.append("\n  Verbose.: " + isVerbose());
+			sb.append("\n  Debug...: " + isDebug());
+			info(sb.toString());
+		}
+	}
+	
+	@Override
+	protected void afterRun(Outline outline) throws Exception
+	{
+		if ( isInfoEnabled() )
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(LOGGING_FINISH);
+			sb.append("\nResults");
+			sb.append("\n  HadError.: " + hadError(outline.getErrorReceiver()));
+			info(sb.toString());
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Run the plugin with and XJC {@link Outline}.
+	 * </p>
+	 * 
+     * <p>
+     * <b>Note:</b> This method is invoked only when a plugin is activated.
+     * </p>
+	 *
+     * @param outline
+     *      This object allows access to various generated code.
+     * 
+     * @return
+     *      If the add-on executes successfully, return true.
+     *      If it detects some errors but those are reported and
+     *      recovered gracefully, return false.
+     *
+     * @throws Exception
+     *      This 'run' method is a call-back method from {@link AbstractPlugin}
+     *      and that method is responsible for handling all exceptions. It reports
+     *      any exception to {@link ErrorHandler} and converts the exception to
+     *      a {@link SAXException} for processing by {@link com.sun.tools.xjc.Plugin}.
+	 */
+	@Override
+	public boolean run(Outline outline) throws Exception
+	{
+		for (ClassOutline classOutline : outline.getClasses())
+			processClassOutline(classOutline);
+		
+		return !hadError(outline.getErrorReceiver());
+	}
+
+	private void processClassOutline(ClassOutline classOutline) throws ClassNotFoundException
+	{
+		final JDefinedClass theClass = classOutline.implClass;
+		for ( Entry<String, JFieldVar> fieldEntry : theClass.fields().entrySet() )
+		{
+			if ( OTHER_ATTRIBUTES.equals(fieldEntry.getValue().name()) )
+			{
+				// Requires that JAR(s) from previous episodes be on the XJC classpath!
+				Class<?> superClass = Class.forName(theClass._extends().fullName());
+				if ( hasField(superClass, OTHER_ATTRIBUTES) )
+				{
+					JMethod method = theClass.getMethod(GET_OTHER_ATTRIBUTES, NO_ARGS);
+					if ( method != null )
+						removeFieldAndMethod(theClass, fieldEntry, method);
+				}
+			}
+		}
+		debug("{}, processClassOutline; Class={}", toLocation(theClass.metadata), theClass.name());
+	}
+
+	private void removeFieldAndMethod(final JDefinedClass theClass, Entry<String, JFieldVar> fieldEntry, JMethod method)
+	{
+		theClass.methods().remove(method);
+		theClass.removeField(fieldEntry.getValue());
+		trace("{}, removeFieldAndMethod; Class={}; Field={}", toLocation(theClass.metadata), theClass.name(), fieldEntry.getValue().name());
+	}
+	
+	// Search the declared fields of the given class and
+	// of all its superclasses and superinterfaces.
+    private boolean hasField(Class<?> clazz, String name)
+    {
+    	if ( clazz != null )
+    	{
+	        for ( Field field : clazz.getDeclaredFields() )
+	        {
+	            if (field.getName().equals(name))
+	                return true;
+	        }
+	        return hasField(clazz.getSuperclass(), name);
+    	}
+    	else
+    		return false;
+    }
+
+}
