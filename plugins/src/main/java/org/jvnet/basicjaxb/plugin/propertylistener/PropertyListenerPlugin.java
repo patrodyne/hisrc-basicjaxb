d5b11b27f274c61616ed20448a204e95451a4e6b
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-09-27 15:23:10 -0400

    Javadoc update

--- plugins/src/main/java/org/jvnet/basicjaxb/plugin/propertylistener/PropertyListenerPlugin.java
+++ plugins/src/main/java/org/jvnet/basicjaxb/plugin/propertylistener/PropertyListenerPlugin.java
@@ -97,9 +97,9 @@ import jakarta.xml.bind.annotation.XmlTransient;
  * <p><b>Veto Strategy</b></p>
  * <p>The {@link org.jvnet.basicjaxb.lang.DefaultVetoStrategy} implements 
  * {@link org.jvnet.basicjaxb.lang.VetoStrategy} to handle {@link java.beans.PropertyVetoException}
- * when it is thrown by your listener. The default action is to log a WARN message and continue.
- * This can be elevated to an ERROR message by setting the log level to
- * <code>org.jvnet.basicjaxb.lang.DefaultVetoStrategy=ERROR</code>. You can configure your own
+ * when it is thrown by your listener. The default action is to log a WARN message and continue
+ * without setting the property. This can be elevated to an ERROR message by setting the log level
+ * to <code>org.jvnet.basicjaxb.lang.DefaultVetoStrategy=ERROR</code>. You can configure your own
  * implementation to implement a handler with other behavior.</p>
  * 
  * @see <a href="https://download.oracle.com/otn-pub/jcp/7224-javabeans-1.01-fr-spec-oth-JSpec/beans.101.pdf">JavaBeans 1.01 Specification</a>
