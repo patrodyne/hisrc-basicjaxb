655909f73c594c787f40965758f014329097b393
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2024-12-23 12:44:28 -0500

    Formatting

--- tools/src/main/java/org/jvnet/basicjaxb/util/OutlineUtils.java
+++ tools/src/main/java/org/jvnet/basicjaxb/util/OutlineUtils.java
@@ -9,45 +9,60 @@ import com.sun.tools.xjc.outline.FieldOutline;
 import com.sun.tools.xjc.outline.Outline;
 import com.sun.tools.xjc.outline.PackageOutline;
 
-public class OutlineUtils {
-
-	private OutlineUtils() {
+/**
+ * Utility methods to get class, field or property info from
+ * a given {@link ClassOutline}
+ */
+public class OutlineUtils
+{
+	/* Seal this utility class */
+	private OutlineUtils()
+	{
 	}
 
-	public static String getPackagedClassName(ClassOutline classOutline) {
+	public static Class<?> getPackagedClass(ClassOutline classOutline)
+		throws ClassNotFoundException
+	{
+		return Class.forName(getPackagedClassName(classOutline));
+	}
+
+	public static String getPackagedClassName(ClassOutline classOutline)
+	{
 		return CodeModelUtils.getPackagedClassName(classOutline.implClass);
 	}
 
-	public static String getClassName(ClassOutline classOutline) {
+	public static String getClassName(ClassOutline classOutline)
+	{
 		return CodeModelUtils.getClassName(classOutline.implClass);
 	}
 
-	public static String getLocalClassName(ClassOutline classOutline) {
+	public static String getLocalClassName(ClassOutline classOutline)
+	{
 		return CodeModelUtils.getLocalClassName(classOutline.implClass);
 	}
 
-	public static String getFieldName(FieldOutline fieldOutline) {
-		return getPackagedClassName(fieldOutline.parent()) + "."
-				+ fieldOutline.getPropertyInfo().getName(true);
+	public static String getFieldName(FieldOutline fieldOutline)
+	{
+		return getPackagedClassName(fieldOutline.parent()) + "." + fieldOutline.getPropertyInfo().getName(true);
 	}
 
-	public static String getPropertyName(FieldOutline fieldOutline) {
-		return NameConverter.standard.toVariableName(fieldOutline
-				.getPropertyInfo().getName(true));
+	public static String getPropertyName(FieldOutline fieldOutline)
+	{
+		return NameConverter.standard.toVariableName(fieldOutline.getPropertyInfo().getName(true));
 	}
 
-	public static String getContextPath(Outline context) {
+	public static String getContextPath(Outline context)
+	{
 		final StringBuilder sb = new StringBuilder();
-
-		for (final Iterator<? extends PackageOutline> packageOutlines = context
-				.getAllPackageContexts().iterator(); packageOutlines.hasNext();) {
+		for ( final Iterator<? extends PackageOutline> packageOutlines = context.getAllPackageContexts()
+			.iterator(); packageOutlines.hasNext(); )
+		{
 			final PackageOutline packageOutline = packageOutlines.next();
 			final String packageName = packageOutline._package().name();
 			sb.append(packageName);
-			if (packageOutlines.hasNext())
+			if ( packageOutlines.hasNext() )
 				sb.append(':');
 		}
 		return sb.toString();
 	}
-
 }
