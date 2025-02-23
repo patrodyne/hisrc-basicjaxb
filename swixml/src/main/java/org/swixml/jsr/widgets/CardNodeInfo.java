2d0db8dd51fa5514483ec85e101ac4011a71f407
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2025-02-23 14:08:03 -0500

    Add nodeStack and cardName properties.

--- swixml/src/main/java/org/swixml/jsr/widgets/CardNodeInfo.java
+++ swixml/src/main/java/org/swixml/jsr/widgets/CardNodeInfo.java
@@ -10,9 +10,22 @@ public class CardNodeInfo
 	public Class<?> getCardClass() { return cardClass; }
 	public void setCardClass(Class<?> cardClass) { this.cardClass = cardClass; }
 	
+	private String cardName;
 	public String getCardName()
 	{
-		return getCardClass().getName();
+		if ( (cardName == null) && (getCardClass() != null) && (getCardClass().getName() != null) )
+			setCardName(getCardClass().getName());
+		return cardName;
+	}
+	public void setCardName(String cardName)
+	{
+		this.cardName = cardName;
+	}
+	
+	public CardNodeInfo(String nodeName, String cardName)
+	{
+		setNodeName(nodeName);
+		setCardName(cardName);
 	}
 	
 	public CardNodeInfo(String nodeName, Class<?> cardClass)
--- swixml/src/main/java/org/swixml/jsr/widgets/MutableTreeModel.java
+++ swixml/src/main/java/org/swixml/jsr/widgets/MutableTreeModel.java
@@ -1,5 +1,7 @@
 package org.swixml.jsr.widgets;
 
+import java.util.Stack;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.MutableTreeNode;
@@ -9,6 +11,18 @@ public class MutableTreeModel
 {
 	private static final long serialVersionUID = 20241201L;
 	
+	private Stack<DefaultMutableTreeNode> nodeStack;
+	public Stack<DefaultMutableTreeNode> getNodeStack()
+	{
+		if ( nodeStack == null )
+			setNodeStack(new Stack<>());
+		return nodeStack;
+	}
+	public void setNodeStack(Stack<DefaultMutableTreeNode> nodeStack)
+	{
+		this.nodeStack = nodeStack;
+	}
+
 	private MutableTreeNode root;
 	@Override
 	public MutableTreeNode getRoot() { return root; }
@@ -23,5 +37,76 @@ public class MutableTreeModel
 	{
 		super(new DefaultMutableTreeNode(rootName));
 		setRoot((MutableTreeNode) super.getRoot());
+	}
+	
+	public DefaultMutableTreeNode pushAdd(DefaultMutableTreeNode node)
+	{
+		if ( getNodeStack().isEmpty() )
+			push(node);
+		else
+		{
+			peekAdd(node);
+			push(node);
+		}
+		return node;
+	}
+	
+	public DefaultMutableTreeNode pushAdd(CardNodeInfo cni)
+	{
+		return pushAdd(new DefaultMutableTreeNode(cni));
+	}
+	
+	public DefaultMutableTreeNode pushAdd(String nodeName, Class<?> cardClass)
+	{
+		return pushAdd(new CardNodeInfo(nodeName, cardClass));
+	}
+	
+	public DefaultMutableTreeNode peekAdd(DefaultMutableTreeNode node)
+	{
+		if ( getNodeStack().isEmpty() )
+			push(node);
+		else
+			peek().add(node);
+		return node;
+	}
+	
+	public DefaultMutableTreeNode peekAdd(CardNodeInfo cni)
+	{
+		return peekAdd(new DefaultMutableTreeNode(cni));
+	}
+	
+	public DefaultMutableTreeNode peekAdd(String nodeName, Class<?> cardClass)
+	{
+		return peekAdd(new CardNodeInfo(nodeName, cardClass));
+	}
+	
+	public DefaultMutableTreeNode push(DefaultMutableTreeNode node)
+	{
+		return getNodeStack().push(node);
+	}
+	
+	public DefaultMutableTreeNode push(CardNodeInfo cni)
+	{
+		return push(new DefaultMutableTreeNode(cni));
+	}
+	
+	public DefaultMutableTreeNode push(String nodeName, Class<?> cardClass)
+	{
+		return push(new CardNodeInfo(nodeName, cardClass));
+	}
+	
+	public DefaultMutableTreeNode push(String nodeName)
+	{
+		return push(new DefaultMutableTreeNode(nodeName));
+	}
+	
+	public DefaultMutableTreeNode pop()
+	{
+		return getNodeStack().pop();
+	}
+	
+	public DefaultMutableTreeNode peek()
+	{
+		return getNodeStack().peek();
 	}
 }
